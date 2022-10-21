0070e1ab14ad64f2248ab3317b87261954dcc38a
000ATSU <99629901+000ATSU@users.noreply.github.com>
2022-10-22 00:18:23 +0900

    [add] new model & controllers
    学習：ルーティングパターン

--- book_admin/config/routes.rb
+++ book_admin/config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+  get 'profiles/edit'
+  get 'profiles/update'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/books/:id" => "books#show"
   delete "/books/:id" => "books#destory"
   resources :publishers
+  resource :profile
+
 
   # Defines the root path route ("/")
   # root "articles#index"
--- book_admin/db/schema.rb
+++ book_admin/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_08_032156) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_21_151528) do
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.string "penname"
@@ -41,6 +41,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_08_032156) do
   create_table "publishers", force: :cascade do |t|
     t.string "name"
     t.text "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
